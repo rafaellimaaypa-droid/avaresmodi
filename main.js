68064d656f7fb27650f2ae17b9e11a001a6cad6e
Rafael <rafael.limaaypa@gmail.com>
2026-08-21 03:24:18 -0300

    Restaurando colisao do editor de objetos e deixando cenario livre

--- main.js
+++ main.js
@@ -5747,12 +5747,19 @@ function adicionarObjeto(scene, x, y, key, angle = 0, scaleX = 1, scaleY = 1, em
 
     if (obj.body) {
         obj.body.enable = shouldHaveCollision;
+        obj.body.immovable = true;
         if (upperKey === 'COLLISION_BOX') {
-            obj.body.immovable = true;
             obj.body.setSize(obj.displayWidth, obj.displayHeight);
             obj.body.setOffset(0, 0);
-            obj.refreshBody();
+        } else {
+            const bodyW = 10;
+            const bodyH = 10;
+            const offsetX = (obj.displayWidth - bodyW) / 2;
+            const offsetY = obj.displayHeight - bodyH;
+            obj.body.setSize(bodyW, bodyH);
+            obj.body.setOffset(offsetX, offsetY);
         }
+        obj.refreshBody();
     }
 
     obj.setInteractive();
@@ -5988,6 +5995,12 @@ function atualizarColisoresMapa(scene) {
         if (player) portaoColliders.push(scene.physics.add.collider(player, portaoLayer));
         if (ogres) portaoColliders.push(scene.physics.add.collider(ogres, portaoLayer));
         if (arrows) portaoColliders.push(scene.physics.add.collider(arrows, portaoLayer, (arrow) => arrow.destroy()));
+    }
+
+    if (monsterObstacles) {
+        if (player) objetosColliders.push(scene.physics.add.collider(player, monsterObstacles));
+        if (ogres) objetosColliders.push(scene.physics.add.collider(ogres, monsterObstacles));
+        if (arrows) objetosColliders.push(scene.physics.add.collider(arrows, monsterObstacles, (arrow) => arrow.destroy()));
     }
 }
 
